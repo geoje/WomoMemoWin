46169440283b15052f92e731b391f87ce8be97fa
Gyeongho Yang <chchch1213@naver.com>
2023-09-15 13:35:55 +0900

    Disappear on alt tab

--- Views/MemoWindow.xaml.cs
+++ Views/MemoWindow.xaml.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using WomoMemo.Models;
 
@@ -15,6 +17,13 @@ namespace WomoMemo.Views
 {
     public partial class MemoWindow : Window
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        private const int GWL_EX_STYLE = -20;
+        private const int WS_EX_APPWINDOW = 0x00040000, WS_EX_TOOLWINDOW = 0x00000080;
+
         bool _loaded = false;
 
         public Memo Memo;
@@ -45,6 +54,11 @@ namespace WomoMemo.Views
         private void window_Loaded(object sender, RoutedEventArgs e)
         {
             _loaded = true;
+
+            //Variable to hold the handle for the form
+            var helper = new WindowInteropHelper(this).Handle;
+            //Performing some magic to hide the form from Alt+Tab
+            SetWindowLong(helper, GWL_EX_STYLE, (GetWindowLong(helper, GWL_EX_STYLE) | WS_EX_TOOLWINDOW) & ~WS_EX_APPWINDOW);
         }
 
         private void window_Activated(object sender, EventArgs e)
