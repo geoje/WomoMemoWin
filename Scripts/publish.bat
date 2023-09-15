233bc5f82e44576a0eb8fc238f8045e035b49b36
Gyeongho Yang <chchch1213@naver.com>
2023-09-15 11:50:30 +0900

    Change publish script to work rmdir

--- Scripts/publish.bat
+++ Scripts/publish.bat
@@ -1,2 +1,1 @@
-gsutil -m cp -r bin/Publish/* gs://womoso.appspot.com/womomemo/windows
-rmdir /s /q bin\Publish
+gsutil -m cp -r bin/Publish/* gs://womoso.appspot.com/womomemo/windows && rmdir /s /q bin\Publish
